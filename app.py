31c6640e32f3824eb87317ddf1c56fb7eedd0285
Laqub Haroon <145701125+Laqub-Haroon@users.noreply.github.com>
2024-06-11 02:23:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def recommend(movie):
     movies_list= sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
     
     recommended_movies = []
-    recommended_movies_posters=[]
+    recommended_movies_posters = []
     for i in movies_list:
         movie_id =movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
